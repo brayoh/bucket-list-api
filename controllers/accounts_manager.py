29cfd0f99402d0af489904b79499fc817183fa35
brayoh <bkuriakb@gmail.com>
2017-07-14 11:18:31 +0300

    Add login resource to handle user authentication for the API

--- controllers/accounts_manager.py
+++ controllers/accounts_manager.py
@@ -5,7 +5,46 @@ from app.models import User
 from app.db_instance import save
 from validator import validate
 
-class AccountsManager(Resource):
+class LoginResource(Resource):
+    """this class handles login and authentication"""
+    def __init__(self):
+        self.parser = reqparse.RequestParser()
+        self.parser.add_argument('username',
+                            type=str,
+                            required=True,
+                            help="username is required")
+        self.parser.add_argument('password',
+                            type=str,
+                            required=True,
+                            help="password is required")
+
+    def post(self):
+        args = self.parser.parse_args(strict=True)
+        username = args.get("username").strip()
+        password = args.get("password").strip()
+
+        if any(arg == "" for arg in [username, password]):
+            message = "username and password is required"
+            status = 400
+        else:
+            user = User.query.filter_by(username=username).first()
+            if user:
+                if user.verify_password(password):
+                    message = "login successful"
+                    status = 200
+                else:
+                    message = "username or password is incorrect"
+                    status = 401
+            else:
+                message = "username or password is incorrect"
+                status = 401
+
+        return make_response(jsonify({
+            "message": message
+        }), status)
+
+
+class RegisterResource(Resource):
     """docstring for AccountsManager."""
     def __init__(self):
         self.parser = reqparse.RequestParser()
