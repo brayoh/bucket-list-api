7902978ac0576274dd45f8ba9ad0daa5e193e216
brayoh <bkuriakb@gmail.com>
2017-07-14 11:03:59 +0300

    Change endpoints to match the recommended routes

--- app/__init__.py
+++ app/__init__.py
@@ -1,17 +1,20 @@
+import os
+import logging.config
 from flask_api import FlaskAPI
 from flask_sqlalchemy import SQLAlchemy
+
+# initialize sql-alchemy
+db = SQLAlchemy()
 
 # import config file
 from config import app_config
 from app.endpoints import bucketlist_blueprint
 
-# initialize sql-alchemy
-db = SQLAlchemy()
 
 def create_app(config_name):
     app = FlaskAPI(__name__, instance_relative_config=True)
     app.config.from_object(app_config[config_name])
-    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
     db.init_app(app)
     app.register_blueprint(bucketlist_blueprint)
+
     return app
--- app/endpoints.py
+++ app/endpoints.py
@@ -1,19 +1,23 @@
 from flask import request, Blueprint
 from flask_restful import Api
 
-from controllers.accounts_manager import AccountsManager
-from controllers.bucketlist import BucketList
+from controllers.accounts_manager import LoginResource, RegisterResource
+from controllers.bucketlist import GetAllBucketLists, GetBucketList
 from controllers.bucketlist_items import BucketListItems
 
 
 bucketlist_blueprint = Blueprint('bucket_list', __name__)
 api = Api(bucketlist_blueprint)
 
-api.add_resource(AccountsManager, '/auth/register', '/auth/login')
-api.add_resource(BucketList,
-                 '/bucketlists',
-                 '/bucketlists/<int:id>')
+# login routes
+api.add_resource(RegisterResource, '/auth/register')
+api.add_resource(LoginResource, '/auth/login')
 
+# bucketlist routes
+api.add_resource(GetAllBucketLists, '/bucketlists')
+api.add_resource(GetBucketList, '/bucketlists/<int:id>')
+
+# bucketlist items routes
 api.add_resource(BucketListItems,
                  '/bucketlists/<int:bucketlist_id>/items',
-                 '/bucketlists/<int:bucketlist_id>/items/<item_id>')
+                 '/bucketlists/<int:bucketlist_id>/items/<int:item_id>')
