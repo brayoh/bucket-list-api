6ce53e1a94800178757509f2c060f971c08cf55c
brayoh <bkuriakb@gmail.com>
2017-07-20 10:37:11 +0300

    Add logging config

--- controllers/accounts_manager.py
+++ controllers/accounts_manager.py
@@ -1,9 +1,12 @@
+import logging
 from flask import jsonify, make_response
 from flask_restful import Resource, reqparse
 
 from app.models import User
 from app.db_instance import save
-from validator import validate
+
+logger = logging.getLogger(__name__)
+
 
 class LoginResource(Resource):
     """this class handles login and authentication"""
