b18fa54ef2515cdb0fffc610cb50f1a993435e2a
brayoh <bkuriakb@gmail.com>
2017-08-06 20:04:40 +0300

    [FEATURE] modify authentication logic to minimize code

--- app/__init__.py
+++ app/__init__.py
@@ -1,5 +1,4 @@
 import os
-import logging.config
 from flask_api import FlaskAPI
 from flask_sqlalchemy import SQLAlchemy
 
@@ -12,7 +11,7 @@ from app.endpoints import bucketlist_blueprint
 
 
 def create_app(config_name):
-    app = FlaskAPI(__name__, instance_relative_config=True)
+    app = FlaskAPI(__name__)
     app.config.from_object(app_config[config_name])
     db.init_app(app)
     app.register_blueprint(bucketlist_blueprint)
--- app/controllers/accounts_manager.py
+++ app/controllers/accounts_manager.py
@@ -1,22 +1,27 @@
+import logging
 from flask import jsonify, make_response
 from flask_restful import Resource, reqparse
 
 from app.models import User
 from app.utils.db import save_data
+from app.utils.auth.token import JWT
+
+logger = logging.getLogger(__name__)
 
 
 class LoginResource(Resource):
     """this class handles login and authentication"""
     def __init__(self):
         self.parser = reqparse.RequestParser()
-        self.parser.add_argument('username',
-                            type=str,
-                            required=True,
-                            help="username is required")
-        self.parser.add_argument('password',
-                            type=str,
-                            required=True,
-                            help="password is required")
+        self.parser.add_argument("username",
+                                 type=str,
+                                 required=True,
+                                 help="username is required")
+
+        self.parser.add_argument("password",
+                                 type=str,
+                                 required=True,
+                                 help="password is required")
 
     def post(self):
         args = self.parser.parse_args(strict=True)
@@ -24,63 +29,66 @@ class LoginResource(Resource):
         password = args.get("password").strip()
 
         if any(arg == "" for arg in [username, password]):
-            message = "username and password is required"
-            status = 400
+            response = ("username and password is required", 400)
         else:
             user = User.query.filter_by(username=username).first()
             if user:
                 if user.verify_password(password):
-                    message = "login successful"
-                    status = 200
+                    token = JWT.create_token(user.id)
+                    response = ("token", token, 200)
                 else:
-                    message = "username or password is incorrect"
-                    status = 401
+                    response = ("message",
+                                "username or password is incorrect", 401)
             else:
-                message = "username or password is incorrect"
-                status = 401
+                response = ("message",
+                            "username or password is incorrect", 401)
 
         return make_response(jsonify({
-            "message": message
-        }), status)
+            response[0]: response[1]
+        }), response[2])
 
 
 class RegisterResource(Resource):
     """docstring for AccountsManager."""
     def __init__(self):
         self.parser = reqparse.RequestParser()
-        self.parser.add_argument('username',
-                            type=str,
-                            required=True,
-                            help="username is required")
-        self.parser.add_argument('password',
-                            type=str,
-                            required=True,
-                            help="password is required")
+        self.parser.add_argument("username",
+                                 type=str,
+                                 required=True,
+                                 help="username is required")
+
+        self.parser.add_argument("password",
+                                 type=str,
+                                 required=True,
+                                 help="password is required")
 
     def post(self):
         args = self.parser.parse_args(strict=True)
-        username = args.get("username")
-        password = args.get("password")
+
+        username = args.get("username").strip()
+        password = args.get("password").strip()
+
+        print(username, password)
 
         if any(arg == "" for arg in [username, password]):
-            message = "username and password is required"
-            status = 400
+            response = ("username and password is required", 400)
+
         elif not username.isalpha():
-            message = "username should not contain special characters"
-            status = 400
+            response = ("username should not contain special characters", 400)
+
         elif len(password) < 6:
-            message = "password should be more than 6 characters"
-            status = 400
+            response = ("password should be more than 6 characters", 400)
+
         elif User.query.filter_by(username=username).first():
-            message = "username already exists"
-            status = 409
+            response = ("username already exists", 409)
+
         else:
             user = User(username, password)
+            # save the user data to the database
             save_data(user)
 
-            message = "user registered successfully"
-            status = 201
+            response = ("user registered successfully", 201)
 
         return make_response(jsonify({
-            "message": message
-        }), status)
+            "message": response[0]
+        }), response[1])
