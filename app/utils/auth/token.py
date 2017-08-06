a1cfc82a6f61064de4c789865e047d8c2eecc196
brayoh <bkuriakb@gmail.com>
2017-08-06 20:05:52 +0300

    [FEATURE] Add JWT class to handle encoding and decoding of user tokens

--- app/utils/auth/token.py
+++ app/utils/auth/token.py
@@ -1,5 +1,28 @@
-def create_token(token):
-    pass
+import os
+from datetime import datetime, timedelta
 
-def validate_token(token):
-    pass
+import jwt
+
+secret = os.getenv("jwt_token_secret")
+
+
+class JWT:
+    @classmethod
+    def create_token(cls, user_id):
+        utcnow = datetime.utcnow()
+        user_data = {
+            'user_id': user_id,
+            'iat': utcnow,
+            'exp': utcnow + timedelta(seconds=int(36000)),
+        }
+        return jwt.encode(user_data,
+                          secret,
+                          algorithm='HS256').decode('utf-8')
+
+    @classmethod
+    def decode_token(cls, token):
+        try:
+            user = jwt.decode(token, secret)
+            return user['user_id']
+        except jwt.exceptions.InvalidTokenError:
+            return "Token has expired, please login again"
