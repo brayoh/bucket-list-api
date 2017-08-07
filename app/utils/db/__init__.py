d95d4da272915ad6a581260679df756bf24a6f4c
brayoh <bkuriakb@gmail.com>
2017-08-07 12:14:15 +0300

    [FIX] Rename save method for database to a more descriptive name

--- app/utils/db/__init__.py
+++ app/utils/db/__init__.py
@@ -4,9 +4,16 @@ from app import db
 logger = logging.getLogger(__name__)
 
 
-def save_data(data):
+def save_record(record):
     try:
-        db.session.add(data)
+        db.session.add(record)
+        db.session.commit()
+    except Exception as err:
+        logger.error(err)
+
+def delete_record(record):
+    try:
+        db.session.delete(record)
         db.session.commit()
     except Exception as err:
         logger.error(err)
