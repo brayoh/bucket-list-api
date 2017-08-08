959852aba8a0e0d1514d7fb8c5bf042049ad42cc
brayoh <bkuriakb@gmail.com>
2017-08-08 16:34:43 +0300

    [CHORE] Add test for edge cases in Authentication resource

--- tests/test_account_manager.py
+++ tests/test_account_manager.py
@@ -1,35 +1,130 @@
+import json
 from base import Base
 
 
-class TestAuthentication(Base):
-    def test_register_user(self):
-        payload = self.client.post("/auth/register", data=self.user)
+class TestAccountsManagerResource(Base):
+    """ This class contains tests for login and registeration
+        of user accounts
+    """
+    # tests for /auth/login
+    def test_wrong_credentials(self):
+        self.client.post("/auth/register",
+                         data=self.user,
+                         content_type='application/json')
 
+        payload = self.client.post("/auth/login",
+                                   data=json.dumps({
+                                       "username": "brian",
+                                       "password": "pass"
+                                   }),
+                                   content_type='application/json')
+        reponse = json.loads(payload.data.decode())
+
+        self.assertEquals(reponse['message'],
+                          "username or password is incorrect")
+        self.assertEquals(payload.status_code, 401)
+
+    def test_logins_user(self):
+        self.client.post("/auth/register",
+                         data=self.user,
+                         content_type='application/json')
+
+        payload = self.client.post("/auth/login",
+                                   data=self.user,
+                                   content_type='application/json')
+
+        reponse = json.loads(payload.data.decode())
+        self.assertIn("token", reponse, "response should contain token")
+        self.assertEquals(payload.status_code, 200)
+
+    def test_empty_login_not_allowed(self):
+        payload = self.client.post("/auth/login",
+                                   data=json.dumps({
+                                       "username": "",
+                                       "password": ""
+                                   }),
+                                   content_type='application/json')
+
+        reponse = json.loads(payload.data.decode())
+        self.assertEquals(reponse['message'],
+                          "username and password is required")
+        self.assertEquals(payload.status_code, 400)
+
+    def test_register_user(self):
+        payload = self.client.post("/auth/register",
+                                   data=self.user,
+                                   content_type='application/json')
+
+        reponse = json.loads(payload.data.decode())
+
+        self.assertEquals(reponse['message'],
+                          "user registered successfully")
         self.assertEquals(payload.status_code, 201)
 
+    # tests for /auth/register
     def test_duplicate_username(self):
-        self.client.post("/auth/register", data=self.user)
-        payload = self.client.post("/auth/register", data=self.user)
+        self.client.post("/auth/register",
+                         data=self.user,
+                         content_type='application/json')
 
+        payload = self.client.post("/auth/register",
+                                   data=self.user,
+                                   content_type='application/json')
+        reponse = json.loads(payload.data.decode())
+
+        self.assertEquals(reponse['message'],
+                          "username already exists")
         self.assertEquals(payload.status_code, 409)
 
     def test_non_registered_user(self):
-        payload = self.client.post("/auth/login", data=self.user)
+        payload = self.client.post("/auth/login",
+                                   data=self.user,
+                                   content_type='application/json')
 
+        reponse = json.loads(payload.data.decode())
+
+        self.assertEquals(reponse['message'],
+                          "username or password is incorrect")
         self.assertEquals(payload.status_code, 401)
 
-    def test_wrong_credentials(self):
-        self.client.post("/auth/register", data=self.user)
-        self.user['password'] = "passwad"
-        payload = self.client.post("/auth/login", data=self.user)
+    def test_special_characters_denied(self):
+        payload = self.client.post("/auth/register",
+                                   data=json.dumps({
+                                       "username": "brian445##$#$",
+                                       "password": "password"
+                                   }),
+                                   content_type='application/json')
 
-        self.assertEquals(payload.status_code, 401)
+        reponse = json.loads(payload.data.decode())
+        self.assertEquals(reponse['message'],
+                          "username should not contain special characters")
+        self.assertEquals(payload.status_code, 400)
 
-    def test_login(self):
-        self.client.post("/auth/register", data=self.user)
-        payload = self.client.post("/auth/login", data=self.user)
+    def test_empty_inputs_not_allowed(self):
+        payload = self.client.post("/auth/register",
+                                   data=json.dumps({
+                                       "username": "",
+                                       "password": ""
+                                   }),
+                                   content_type='application/json')
 
-        self.assertEquals(payload.status_code, 200)
+        reponse = json.loads(payload.data.decode())
+        self.assertEquals(reponse['message'],
+                          "username and password is required")
+        self.assertEquals(payload.status_code, 400)
+
+    def test_password_length(self):
+        payload = self.client.post("/auth/register",
+                                   data=json.dumps({
+                                       "username": "brian",
+                                       "password": "pass"
+                                   }),
+                                   content_type='application/json')
+
+        reponse = json.loads(payload.data.decode())
+        self.assertEquals(reponse['message'],
+                          "password should be more than 6 characters")
+        self.assertEquals(payload.status_code, 400)
 
 
 if __name__ == "__main__":
