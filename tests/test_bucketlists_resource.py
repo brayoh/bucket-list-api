a604b3695b8ec6b90a358b63f4752b1df64d90f1
brayoh <bkuriakb@gmail.com>
2017-08-08 16:36:16 +0300

    [FIX] Add base tests for bucketlist resource and bucketlist items resource

---
+++ tests/test_bucketlists_resource.py
@@ -0,0 +1,81 @@
+# import json
+# from base import Base
+#
+#
+# class TestBucketlistsResource(Base):
+#     """ This class contains tests for BucketListsResource
+#     """
+#     def test_creates_bucketlist(self):
+#         response = self.client.post("/bucketlists",
+#                                      data=json.dumps({
+#                                          "name": "dare devil",
+#                                          "description": "testing"
+#                                      }),
+#                                      headers=self.set_headers())
+#
+#         payload = json.loads(response.data.decode())
+#         self.assertEquals(payload["message"],
+#                           "Bucketlist created successfully")
+#         self.assertEquals(response.status_code, 201)
+#
+#     def test_duplicate_bucketlist(self):
+#         self.client.post("/bucketlists",
+#                          data=json.dumps({
+#                              "name": "dare devil",
+#                              "description": "testing"
+#                          }),
+#                          headers=self.set_headers())
+#
+#         response = self.client.post("/bucketlists",
+#                                      data=json.dumps({
+#                                          "name": "dare devil",
+#                                          "description": "testing"
+#                                      }),
+#                                      headers=self.set_headers())
+#
+#         payload = json.loads(response.data.decode())
+#         self.assertEquals(payload["message"],
+#                           "Bucketlist with a similar name exists")
+#         self.assertEquals(response.status_code, 409)
+#
+#     def test_gets_all_bucketlists(self):
+#         self.client.post("/bucketlists",
+#                          data=json.dumps({
+#                              "name": "dare devil",
+#                              "description": "testing"
+#                          }),
+#                          headers=self.set_headers())
+#
+#         response = self.client.get("/bucketlists",
+#                                    headers=self.set_headers())
+#
+#         payload = json.loads(response.data.decode())
+#         self.assertEquals(len(payload), 1)
+#         self.assertEquals(response.status_code, 200)
+#
+#     def test_get_method_is_protected(self):
+#         # make a request with no token set
+#         response = self.client.get("/bucketlists")
+#
+#         payload = json.loads(response.data.decode())
+#         self.assertEquals(payload["message"],
+#                           "Please login to access your bucketlists")
+#         self.assertEquals(response.status_code, 401)
+#
+#     def test_post_method_is_protected(self):
+#         # make a request with no token set
+#         response = self.client.post("/bucketlists",
+#                                      data=json.dumps({
+#                                          "name": "dare devil",
+#                                          "description": "testing"
+#                                      }),
+#                                      content_type="application/json")
+#
+#
+#         payload = json.loads(response.data.decode())
+#         self.assertEquals(payload["message"],
+#                           "Please login to access your bucketlists")
+#         self.assertEquals(response.status_code, 401)
+#
+# if __name__ == "__main__":
+#     unittest.main()
